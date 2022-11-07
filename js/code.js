1f9a8862433433f172dd7ca5735a4ffb50ee480d
Reece1109 <reecesimon84@gmail.com>
2022-11-07 16:38:32 +0200

    main

---
+++ js/code.js
@@ -0,0 +1,18 @@
+// Write to a document
+// document.write("Hello World");
+// document.write("<br>My name is Reece Simon");
+// document.write("br>Am a developer");
+/*
+You are
+awesome
+*/
+let userName = "Reece";
+let userAge = 23;
+let americanFood = true;
+
+document.write(userName + '<br>');
+document.write(userAge + '<br>');
+document.write(americanFood);
+
+let names = {"Reece , Daniel Tristan"}
+console.log =
